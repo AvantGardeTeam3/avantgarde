ec48813a32c48a5f80310a69ef25c20e24396835
Choi Lam Wong <julian.wcl@outlook.com>
2019-11-30 21:06:07 +0000

    added transformation for fleur

---
+++ avantgarde/avantgarde/Fleur.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input.Inking;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace avantgarde
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Fleur : Page
+    {
+        private Point inkCanvasCentre;
+        List<InkStroke> userStrokes = new List<InkStroke>();
+        private InkStrokeBuilder inkStrokeBuilder;
+
+        public Fleur()
+        {
+            this.InitializeComponent();
+            inkCanvas.InkPresenter.InputDeviceTypes =
+                Windows.UI.Core.CoreInputDeviceTypes.Mouse |
+                Windows.UI.Core.CoreInputDeviceTypes.Pen |
+                Windows.UI.Core.CoreInputDeviceTypes.Touch;
+            inkCanvas.InkPresenter.StrokesCollected += InkPresenter_StrokesCollected;
+            inkCanvasCentre.X = canvas.ActualWidth / 2;
+            inkCanvasCentre.Y = canvas.ActualHeight / 2;
+            Debug.WriteLine("{0} : {1}", inkCanvasCentre.X, inkCanvasCentre.Y);
+            inkStrokeBuilder = new InkStrokeBuilder();
+        }
+        private void InkPresenter_StrokesCollected(InkPresenter sender, InkStrokesCollectedEventArgs e)
+        {
+            IReadOnlyList<InkStroke> strokes = e.Strokes;
+            foreach (InkStroke stroke in strokes)
+            {
+                userStrokes.Add(stroke);
+            }
+            inkCanvas.InkPresenter.StrokeContainer.Clear();
+            inkCanvas.InkPresenter.StrokeContainer.AddStrokes(this.Transfrom(userStrokes));
+        }
+        private List<InkStroke> Transfrom(List<InkStroke> u)
+        {
+            List<InkStroke> transformedStrokes = new List<InkStroke>();
+            foreach (InkStroke stroke in u)
+            {
+                transformedStrokes.AddRange(TransformStroke(stroke, 8));
+            }
+            return transformedStrokes;
+        }
+        private List<InkStroke> TransformStroke(InkStroke stroke, int num)
+        {
+            IReadOnlyList<InkPoint> inkPoints = stroke.GetInkPoints();
+            List<InkStroke> ret = new List<InkStroke>();
+            double thetaDiff = Math.PI * 2 / num;
+            for(int i = 0; i < num; i++)
+            {
+                List<InkPoint> transformedInkPoints = new List<InkPoint>();
+                foreach(InkPoint ip in inkPoints)
+                {
+                    Single pressure = ip.Pressure;
+                    Point transformedPoint = TransformPoint(ip.Position, thetaDiff * i);
+                    transformedInkPoints.Add(new InkPoint(transformedPoint, pressure));
+                }
+                ret.Add(inkStrokeBuilder.CreateStrokeFromInkPoints(transformedInkPoints, System.Numerics.Matrix3x2.Identity));
+            }
+            return ret;
+        }
+        private Point TransformPoint(Point point, double thetaDiff)
+        {
+            Point relPoint = ToRelative(point);
+            Point polarPoint = ToPolar(relPoint);
+            polarPoint.Y += thetaDiff;
+            return ToAbsolute(ToCoordinate(polarPoint));
+        }
+        private Point ToPolar(Point point)
+        {
+            double distance = Math.Sqrt(Math.Pow(point.X, 2) + Math.Pow(point.Y, 2));
+            double x = point.X;
+            double y = point.Y;
+            double theta;
+            if (x == 0 && y == 0)
+                return new Point(distance, 0);
+            if (y == 0 && x > 0)
+                return new Point(distance, Math.PI / 2);
+            if (y == 0 && x < 0)
+                return new Point(distance, 3 / 2 * Math.PI);
+            if (x == 0 && y > 0)
+                return new Point(distance, 0);
+            if (x == 0 && y < 0)
+                return new Point(distance, Math.PI);
+
+            if (x > 0 && y > 0)
+                theta = Math.Atan(x / y);
+            else if (x > 0 && y < 0)
+                theta = Math.PI - Math.Atan(-x / y);
+            else if (x < 0 && y > 0)
+                theta = 2 * Math.PI - Math.Atan(-x / y);
+            else
+                theta = Math.Atan(x / y) + Math.PI;
+
+            return new Point(distance, theta);
+        }
+        private Point ToCoordinate(Point point)
+        {
+            Point ret = new Point();
+            double theta = point.Y;
+            double r = point.X;
+            ret.X = r * Math.Sin(theta);
+            ret.Y = r * Math.Cos(theta);
+            return ret;
+        }
+        private Point ToRelative(Point point)
+        {
+            inkCanvasCentre.X = canvas.ActualWidth / 2;
+            inkCanvasCentre.Y = canvas.ActualHeight / 2;
+            return new Point(point.X - inkCanvasCentre.X, inkCanvasCentre.Y - point.Y);
+        }
+        private Point ToAbsolute(Point point)
+        {
+            inkCanvasCentre.X = canvas.ActualWidth / 2;
+            inkCanvasCentre.Y = canvas.ActualHeight / 2;
+            return new Point(point.X + inkCanvasCentre.X, inkCanvasCentre.Y - point.Y);
+        }
+    }
+}
